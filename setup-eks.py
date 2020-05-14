70db130e3985461d5494bba0c23b2dde9d65b9e1
Wilson Waterman <hwaterman@precima.com>
2020-05-14 16:06:55 -0400

    new efs id

--- setup-eks.py
+++ setup-eks.py
@@ -1,3 +1,14 @@
 import cliscript
+import subprocess
+import fileinput
+import sys
 
-cliscript.run_cli_cmd("ww","aws ec2 describe-vpcs")
+#cliscript.run_cli_cmd("ww","aws ec2 describe-vpcs")
+#cliscript.run_cli_cmd("ww","eksctl create cluster -f infra-setup/create-cluster.yaml")
+#cliscript.run_cli_cmd("ww","kubectl apply -f secrets/secret.yaml")
+#cliscript.run_cli_cmd("ww","kubectl apply -k 'github.com/kubernetes-sigs/aws-efs-csi-driver/deploy/kubernetes/overlays/stable/?ref=master'")
+#proc = subprocess.Popen(cliscript.run_cli_cmd("ww","aws ec2 create-security-group --group-name eks-efs-sg --description connect-to-efs --output text"), shell=True, stdout=subprocess.PIPE, )
+#SG_ID = proc.communicate()[0]
+#print(SG_ID)
+#VPC_ID = cliscript.run_cli_cmd("ww","aws eks describe-cluster --name react-app --query cluster.resourcesVpcConfig.vpcId --output text")
+#SG_ID = cliscript.run_cli_cmd("ww","aws ec2 create-security-group --group-name eks-efs-sg --description connect-to-efs --output text")
