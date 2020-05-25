57d11f18fe00d295066983866e3f00a8333887c1
Wilson Waterman <hwaterman@precima.com>
2020-05-25 11:19:21 -0400

    move terraform learning to a new repo

--- terraform/example.tf
+++
@@ -1,9 +0,0 @@
-provider "aws" {
-    profile         = "default"
-    region          = "us-east-1"
-}
-
-resource "aws_instance" "example" {
-    ami             = "ami-085925f297f89fce1"
-    instance_type   = "t2.micro"
-}
